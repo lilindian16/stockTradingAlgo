2dd101135e813e0694b982484558b57ec1ef4450
Jaime Sequeira <jaime@astutesmartlocks.com>
2021-02-17 20:55:25 +1300

    This marks the start of our success

---
+++ tradingAlgo.py
@@ -0,0 +1,19 @@
+"""
+    Leshgo
+"""
+
+print('Hello World')
+
+
+def sayHello():
+
+    name = input("Please enter your name here: ")
+    print("Hello " + name + " and welcome to the start of our algo")
+
+
+def main():
+
+    sayHello()
+
+
+main()
