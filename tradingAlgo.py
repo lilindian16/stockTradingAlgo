a5abec95e5869b2c9046deec7bf0d2bbb3d616cc
Jaime Sequeira <jaime@astutesmartlocks.com>
2021-03-02 21:13:20 +1300

    Push info straight into broker object

--- tradingAlgo.py
+++ tradingAlgo.py
@@ -8,12 +8,13 @@ import alpaca_trade_api as tradeapi
 
 class brokerAccount:
 
-    def __init__(self, name, apiKey, apiSecretKey, baseURL):
+    def __init__(self, name, apiKey, apiSecretKey, baseURL, info):
 
         self.name = name
         self.apiKey = apiKey
         self.apiSecretKey = apiSecretKey
         self.base_url = baseURL
+        self.info = info
 
 
 # Authentication and connection details
@@ -22,12 +23,14 @@ alpacaAPISecret = 'sKQDrJ2HKWBtez3JZtUSC7sFJKF066e0MIo1F6fK'
 alpacaBaseURL = 'https://paper-api.alpaca.markets'
 
 # Instance of the broker account class
-alpacaAccount = brokerAccount("Alpaca", alpacaAPIKey, alpacaAPISecret, alpacaBaseURL)
+alpacaAccount = brokerAccount("Alpaca", alpacaAPIKey, alpacaAPISecret, alpacaBaseURL, 0)
 
 # Instantiate REST API for the Alpaca broker
 alpacaAPI = tradeapi.REST(alpacaAccount.apiKey, alpacaAccount.apiSecretKey, 
     alpacaAccount.base_url, api_version='v2')
 
 # obtain account information
-alpacaAccountInfo = alpacaAPI.get_account()
-print(alpacaAccountInfo)
+alpacaAccount.info = alpacaAPI.get_account()
+print(alpacaAccount.info)
+
+
